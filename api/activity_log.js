70c63719dd98ac50305a85712e844b281ff0c254
jordy <jordyvigo@hotmail.com>
2025-04-14 18:33:46 -0500

    rep6

--- api/activity_log.js
+++ api/activity_log.js
@@ -1,5 +1,5 @@
 // pages/api/activity_log.js
-import clientPromise from '../../lib/mongodb';
+import clientPromise from '../lib/mongodb';
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
